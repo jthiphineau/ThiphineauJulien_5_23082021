99801f0c75a7239b23ee8cb2b975d2074ef5d83e
Julien Thiphineau <j.thiphineau@yahoo.fr>
2021-08-31 16:01:18 +0200

    Image

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,35 +1,37 @@
 // AFFICHAGE HTML PRODUITS INDEX
-const productsView = item => {
-    container = document.getElementById("container")
-    .innerHTML +=   `<div class="col mb-5">
+const productsView = (item) => {
+  container = document.getElementById(
+    "container"
+  ).innerHTML += `<div class="col mb-5">
                         <div class="card h-100">
-                            <img class="card-img-top" src=${item.imageUrl} alt="..." />
+                            <img class="card-img-top" src=${
+                              item.imageUrl
+                            } alt="..." />
                             <div class="card-body p-4">
                                 <div class="text-center">
                                     <h5 class="fw-bolder">${item.name}</h5>
-                                    <span>${item.price/100}€</span>
+                                    <span>${item.price / 100}€</span>
                                 </div>
                             </div>
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="./pages/item_page.html?id=${item.id}">Voir fiche produit</a></div>
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="./pages/item_page.html?id=${
+                                  item.id
+                                }">Voir fiche produit</a></div>
                             </div>
                         </div>
-                    </div>`                                                    
+                    </div>`;
 };
-
 
 // REQUETE API
 fetch("http://localhost:3000/api/cameras")
-    .then(res => res.json())
-    .then(function (ProductsList) {
-        for (let product of ProductsList) {
-            let item = new Product(product)
-            productsView(item);   
-        }
-    })
-    .catch(function (err) {
-        console.log("fetch Error")
-        alert("Désolé aucun produit n'a été trouvé !")
-    });
-
-    
+  .then((res) => res.json())
+  .then(function (ProductsList) {
+    for (let product of ProductsList) {
+      let item = new Product(product);
+      productsView(item);
+    }
+  })
+  .catch(function (err) {
+    console.log("fetch Error");
+    alert("Désolé aucun produit n'a été trouvé !");
+  });
